4d9552201be96375332c5a6cf5558d2a950ae067
Greg Hering <ghering90@gmail.com>
2024-11-02 16:28:29 -0700

    add spaceship sprite

--- main.py
+++ main.py
@@ -3,16 +3,22 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
+    player = Player(x=SCREEN_WIDTH/2,y=SCREEN_HEIGHT/2)
+    time = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
+        player.draw(screen)
         pygame.display.flip()
+        dt = time.tick(60) / 1000
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
